3ab6a329e2033f1b8a67bb0fd718a94013dd63f4
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-04-16 13:38:10 +0300

    Wrap application layout in a div with Jakarta font styling

--- smartchain_hub_frontend/src/pages/_app.tsx
+++ smartchain_hub_frontend/src/pages/_app.tsx
@@ -2,13 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { Web3Provider } from "@/context/Web3Context";
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Web3Provider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <div className={jakarta.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </Web3Provider>
   );
 }

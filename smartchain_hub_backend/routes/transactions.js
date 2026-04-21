b16550224163d2095b8991f2bdc522cf660671e0
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-04-21 18:50:28 +0300

    refactor: remove unused Node.js backend to eliminate architectural redundancy

--- smartchain_hub_backend/routes/transactions.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/transactionController');
-
-router.get('/:userId', ctrl.getTransactions);
-router.post('/', ctrl.createTransaction);
-router.post('/optimize', ctrl.optimizeTransaction);
-
-module.exports = router;
--- smartchain_hub_backend/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/userController');
-
-router.get('/:userId', ctrl.getProfile);
-router.put('/:userId', ctrl.updateProfile);
-
-module.exports = router;
